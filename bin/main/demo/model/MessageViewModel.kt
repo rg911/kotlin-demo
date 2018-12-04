f88c6db0aa783d1c60d55937094c0b11290e2042
Steven Liu <steven.liu@skybettingandgaming.com>
2018-12-04 01:02:35 +0000

    removed bin

--- bin/main/demo/model/MessageViewModel.kt
+++
@@ -1,9 +0,0 @@
-package demo.model
-
-import com.fasterxml.jackson.annotation.JsonInclude
-
-@JsonInclude(JsonInclude.Include.NON_NULL)
-data class MessageViewModel<T>(
-        val header: Header,
-        val body: T
-)
--- bin/main/demo/service/ITransformerService.kt
+++
@@ -1,9 +0,0 @@
-package demo.service
-
-import demo.model.MessageViewModel
-
-
-interface ITransformerService {
-    fun parse(input: String): String
-
-}
